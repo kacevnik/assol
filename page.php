bd3c6bb763f1b79d6568ea7ac00aedbcf7772d0f
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-02-10 21:22:54 +0300

    Доработка области контента для главной

--- page.php
+++ page.php
@@ -6,14 +6,14 @@
  */
 get_header(); // подключаем header.php ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
-<div id='content' class="container <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<h1><?php the_title(); // заголовок поста ?></h1>
-						<?php the_content(); // контент ?>
-				
-			</div>
-			<?php endwhile; // конец цикла ?>
-			</div>
+	<div id='content' class="container <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<h1><?php the_title(); // заголовок поста ?></h1>
+						<?php the_content(); // контент ?>				
+		</div>
+<?php endwhile; // конец цикла ?>
+	</div>
+
 <div class="container">
 	<div class="gallery">
 		<div class="gallery-container">
@@ -63,42 +63,5 @@ get_header(); // подключаем header.php ?>
 		</div>
 	</div>
 </div>
-		</main>
-		<noindex>
-	<style>
-		footer {
-			height: 120px;
-			background: url('img/footer.png');
-			font-size: 12px;
-			color: #8f8f8f;
-		}
-		footer a {
-			color: #8f8f8f;
-		}
-		footer .copy {
-			float: left;
-			padding-top: 20px;
-		}
-		footer .copy div {
-			height: 25px;
-		}
-		footer .phones {
-			float: right;
-			padding-top: 20px;
-			text-align: right;
-		}
-		footer .phones .made {
-			padding-right: 120px;
-			position: relative;
-			margin-top: 10px;
-			text-align: left;
-		}
-		footer .phones .made img {
-			position: absolute;
-			right: 0;
-			bottom: 0;
-		}
-	</style>
-</noindex>
-
+</main>
 <?php get_footer(); // подключаем footer.php ?>
