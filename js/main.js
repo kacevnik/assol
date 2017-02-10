53b2e77ae77954f9c024aa50669082cb60e441df
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-02-10 20:24:22 +0300

    Не большие подправления

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// custom scripts
+$( document ).ready(function() {
+	$('.gsInformer').hover(function(){
+		$(this).animate({left: "0px"}, 600);
+	}, function(){
+		$(this).animate({left: "-155px"}, 600);
+	});
+});
