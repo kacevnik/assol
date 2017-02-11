741b05be4634891f5c60c7e8408b28e6a4cb7426
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-02-12 00:11:04 +0300

    Исправление слайдера

--- header.php
+++ header.php
@@ -102,30 +102,30 @@
 			<!-- Wrapper for slides -->
 				<div class="carousel-inner">
 					<div class="item active">
-						<img src="<?php bloginfo( 'template_url' ); ?>/img/slide1.png" alt="">
+						<img src="<?php bloginfo( 'template_url' ); ?>/img/slider1.jpg" alt="">
 					</div>
 
 
 					<div class="item">
-						<img src="<?php bloginfo( 'template_url' ); ?>/img/slide2.png" alt="">
+						<img src="<?php bloginfo( 'template_url' ); ?>/img/slider2.jpg" alt="">
 					</div>
 
 
 					<div class="item">
-						<img src="<?php bloginfo( 'template_url' ); ?>/img/slide3.png" alt="">
+						<img src="<?php bloginfo( 'template_url' ); ?>/img/slider3.jpg" alt="">
 					</div>
 
 
 					<div class="item">
-						<img src="<?php bloginfo( 'template_url' ); ?>/img/slide4.png" alt="">
+						<img src="<?php bloginfo( 'template_url' ); ?>/img/slider4.jpg" alt="">
 					</div>
 				</div>
 
 			<!-- Controls -->
 			<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-
+				<span class="glyphicon glyphicon-chevron-left"></span>
 			</a>
 			<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-
+				<span class="glyphicon glyphicon-chevron-right"></span>
 			</a>
 			</div>
